1cf78d31a4a24ca381fd6ef3178d89423a9ddc32
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-06-13 23:36:47 +0200

    PHP 7.2 - boost bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -11,13 +11,15 @@
 
 use Symfony\Polyfill\Php72 as p;
 
-if ('\\' === DIRECTORY_SEPARATOR && !function_exists('sapi_windows_vt100_support')) {
-    function sapi_windows_vt100_support() { return false; }
-}
-if (!function_exists('stream_isatty')) {
-    function stream_isatty($stream) { return function_exists('posix_isatty') && @posix_isatty($stream); }
-}
-if (!function_exists('utf8_encode')) {
-    function utf8_encode($s) { return p\Php72::utf8_encode($s); }
-    function utf8_decode($s) { return p\Php72::utf8_decode($s); }
+if (PHP_VERSION_ID < 70200) {
+    if ('\\' === DIRECTORY_SEPARATOR && !function_exists('sapi_windows_vt100_support')) {
+        function sapi_windows_vt100_support() { return false; }
+    }
+    if (!function_exists('stream_isatty')) {
+        function stream_isatty($stream) { return function_exists('posix_isatty') && @posix_isatty($stream); }
+    }
+    if (!function_exists('utf8_encode')) {
+        function utf8_encode($s) { return p\Php72::utf8_encode($s); }
+        function utf8_decode($s) { return p\Php72::utf8_decode($s); }
+    }
 }
